a2e86018304df71605d2496901aa61ae5ce95eb1
katemba fahad <katsfahad008@gmail.com>
2024-10-16 12:35:59 +0300

    Create user model and migrate

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+app.use(express.json())
+app.use(morgan('dev'))
 
+
+app.get('/', (req,res)=>{
+    res.send('Working')
+})
 
 
 const PORT  = 4300
-app.listen(()=>{
-    console.log(`http://localhost:${PORT}`)
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`)
 })
