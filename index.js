f884f7f85671cb5bc708d5861c7d6ef8a3f52ddd
katemba fahad <katsfahad008@gmail.com>
2024-10-19 14:14:49 +0300

    Install cors to allow api on front end

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const userRouter = require('./Routes/useRouter')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
